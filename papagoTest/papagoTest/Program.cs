7e0bc10c6c20d72d6c0ba0e9c376cfe228719211
jinkoooooo <52557230+jinkoooooo@users.noreply.github.com>
2019-07-19 13:34:34 +0900

    papago
    test

---
+++ papagoTest/papagoTest/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace papagoTest
+{
+
+    
+
+    class Program
+    {
+
+        public static string idKey = "qBE7Bt05mNteUf2STtAW";
+
+        public static string secretKey = "Vgbc6FBobo";
+        static void Main(string[] args)
+        {
+            string url = "https://openapi.naver.com/v1/papago/detectLangs";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Headers.Add("X-Naver-Client-Id", idKey);
+            request.Headers.Add("X-Naver-Client-Secret", secretKey);
+            request.Method = "POST";
+            string query = "오늘 날씨는 어떻습니까?";
+            byte[] byteDataParams = Encoding.UTF8.GetBytes("query=" + query);
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = byteDataParams.Length;
+            Stream st = request.GetRequestStream();
+            st.Write(byteDataParams, 0, byteDataParams.Length);
+            st.Close();
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream stream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+            string text = reader.ReadToEnd();
+            stream.Close();
+            response.Close();
+            reader.Close();
+            Console.WriteLine(text);
+        }
+    }
+}
