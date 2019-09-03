ffd13944a4a290fc423475879881c5dd1c953da6
jinkoooooo <52557230+jinkoooooo@users.noreply.github.com>
2019-09-03 15:44:48 +0900

    asdf

--- Final_Code/serverForm/serverForm/mapperDB.cs
+++ Final_Code/serverForm/serverForm/mapperDB.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using MySql.Data.MySqlClient;
+using MySql.Data.MySqlClient;//dapper
 
 namespace serverForm
 {
